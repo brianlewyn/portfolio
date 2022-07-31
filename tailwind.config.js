8018b60bade76abe608b0c8becd6d7dcbabc4d9d
brianlewyn <brianlewyn@gmail.com>
2022-07-31 03:34:49 -0500

    Add Comfortaa & Poppins fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Comfortaa: 'Comfortaa',
+        Poppins: 'Poppins',
+      },
+    },
   },
   plugins: [],
 }
